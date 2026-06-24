9bf1d5728dbf1239d87cb302dc7433d3bc716f72
Ivan Korotaev <ivan@latenode.com>
2026-06-24 15:47:12 -0600

    Fix: include lat/lng in HEX_GRID so label markers can be created

--- scripts/build_map.py
+++ scripts/build_map.py
@@ -74,6 +74,7 @@ for tid, info in tashkent_grid['hexes'].items():
     if h3_cell in rec_set: z = 'recommended'
     elif h3_cell in forb_set: z = 'not_allowed'
     raw_metrics[tid] = {
+        'lat': lat, 'lng': lng,            # needed for label markers in JS
         'pop': pop, 'd_pvz': round(d_pvz),
         'n_listings': n_listings, 'n_first': n_first,
         'frac_first': round(frac_first, 3),
